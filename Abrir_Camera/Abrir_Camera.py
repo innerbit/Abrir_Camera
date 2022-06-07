7786a3ba41f0f7a97708fd7797372198a53a0e76
innerbit <suporte@innerbit.com.br>
2022-06-07 10:44:40 -0300

    Adicionar arquivos de projeto.

---
+++ Abrir_Camera/Abrir_Camera.py
@@ -0,0 +1,20 @@
+#Bibliotecas
+
+import pyautogui
+
+
+#Inicio Projeto
+
+
+#Modo Tela Cheia
+pyautogui.press('f10')
+pyautogui.sleep(1)
+pyautogui.press('right')
+pyautogui.press('right')
+pyautogui.press('down')
+pyautogui.press('down')
+pyautogui.press('down')
+pyautogui.press('down')
+pyautogui.press('down')
+pyautogui.press('enter')
+pyautogui.sleep(2)
